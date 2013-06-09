a416039c5d8e7db0580b6a7720a3feec1c68dd49
tylergoelz <tyler@tylergoelz.com>
2013-06-09 15:09:44 -0400

    fixed the approach section for responsive screen sizes

--- index.php
+++ index.php
@@ -17,70 +17,81 @@
 		    			<div class="topic-image">
 		    				<img src="images/icons/Clipboard@2x.png" alt="Clipboard">
 		    			</div>
-		    			<h3>Customer Validation</h3>
-		    			<p>Envision the customer, conduct quality interviews and learn from customer development.</p>
+		    			<div class="frame">
+			    			<h3>Customer Validation</h3>
+			    			<p>Envision the customer, conduct quality interviews and learn from customer development.</p>
+		    			</div>
 		    		</div>
 		    		<div class="span6 topic">
 		    			<div class="topic-image">
 		    				<img src="images/icons/Bullseye@2x.png" alt="Clipboard">
 		    			</div>
-		    			<h3>Product Market Fit</h3>
-		    			<p>Use a process of design, distribution and data collection to find messaging, pricing & audience.</p>
+		    			<div class="frame">
+			    			<h3>Product Market Fit</h3>
+			    			<p>Use a process of design, distribution and data collection to find messaging, pricing & audience.</p>
+		    			</div>
 		    		</div>
 		    		<div class="span6 topic">
 		    			<div class="topic-image">
 		    				<img src="images/icons/Tools@2x.png" alt="Clipboard">
 		    			</div>
-		    			<h3>Product Design</h3>
-		    			<p>Deliver useful & usable products through Lean UX - a combination of Lean Startup and User Experience.</p>
+		    			<div class="frame">
+			    			<h3>Product Design</h3>
+							<p>Deliver products through Lean UX - a combo of Lean Startup and User Experience.</p>
+		    			</div>		    			
 		    		</div>
 		    		<div class="span6 topic">
 		    			<div class="topic-image">
 		    				<img src="images/icons/Touch@2x.png" alt="Clipboard">
 		    			</div>
-		    			<h3>User Testing</h3>
-		    			<p>Put assumptions to the test. Discover opportunities that could serve user’s needs and inform the rest of the design process.</p>
+		    			<div class="frame">
+		    				<h3>User Testing</h3>
+		    				<p>Discover opportunities that could serve user’s needs and inform the rest of the design process.</p>
+		    			</div>		    			
 		    		</div>
 		    		<div class="span6 topic">
 		    			<div class="topic-image">
 		    				<img src="images/icons/Chart@2x.png" alt="Clipboard">
 		    			</div>
-		    			<h3>Scalability</h3>
-		    			<p>Start, design, optimize and scale products and business models.</p>
+		    			<div class="frame">
+		    				<h3>Scalability</h3>
+			    			<p>Start, design, optimize and scale products and business models.</p>
+		    			</div>		    			
 		    		</div>
 		    		<div class="span6 topic">
 		    			<div class="topic-image">
 		    				<img src="images/icons/Baggage@2x.png" alt="Clipboard">
 		    			</div>
-		    			<h3>Lifestyle Design</h3>
-		    			<p>“Life doesn’t have to be so damn hard. It really doesn’t.” - <a href="http://www.fourhourworkweek.com/blog/?ref=thefaceoftylergoelz" target="_blank">Tim Ferriss</a></p>
+		    			<div class="frame">
+			    			<h3>Lifestyle Design</h3>
+			    			<p>“Life doesn’t have to be so damn hard. It really doesn’t.” - <a href="http://www.fourhourworkweek.com/blog/?ref=thefaceoftylergoelz" target="_blank">Tim Ferriss</a></p>
+		    			</div>		    			
 		    		</div>
 		    	</div>
 		    </div>
 		</section>
 		<section id="muse" class="muse-section">
-		
-		    		<span class="muse">
-		    			<span class="details">
-		    			    <span class="frame">
-		    			    	<h2>Redwood Agile</h2>
-		    			        <p>Redwood Agile is my current Muse*. We help entrepreneurs start, design, grow, optimize, and scale their products and business models</p>
-		    			    </span>
-		    			</span>
-						<span class="definition">
-		    				<span class="frame">
-				    			<p><small>*Muse /myooz/ (noun)</small></p>
-				    			<p><small>A low-maintenance business that generates significant income.</small></p>
-		    				</span>
-		    			</span>
-		    			<a href="http://www.redwoodagile.com/?ref=tylergoelz" target="_blank" class="web">
-		    			    <img src="/images/redwood_website.png" alt="Basic Elements" width="552" height="408">
-		    			    <img src="http://designmodo.com/wp-content/themes/designmodov2/flat-ui/images/eye-icon.png" class="icon">
-		    			</a>
-		    			<div class="buttonholder">
-		    			    <a href="" class="btn btn-primary btn-large"><span class="fui-eye"></span> View Redwood Agile</a>
-		    			</div>
-		    		</span>
+		    <span class="muse">
+		        <span class="details">
+		            <span class="frame">
+		            	<h2>Redwood Agile</h2>
+		                <p>Redwood Agile is my current Muse*. We help entrepreneurs start, design, grow, optimize, and scale their products and business models</p>
+		            </span>
+		        </span>
+			    <span class="definition">
+		        	<span class="frame">
+			    		<p><small>*Muse /myooz/ (noun)</small></p>
+			    		<p><small>A low-maintenance business that generates significant income.</small></p>
+		        	</span>
+		        </span>
+		        <a href="http://www.redwoodagile.com/?ref=tylergoelz" target="_blank" class="web">
+		            <img src="/images/redwood_website.png" alt="Basic Elements" width="552" height="408">
+		            <img src="http://designmodo.com/wp-content/themes/designmodov2/flat-ui/images/eye-icon.png" class="icon">
+		        </a>
+		        <div class="buttonholder">
+		            <a href="" class="btn btn-primary btn-large"><span class="fui-eye"></span> View Redwood Agile</a>
+		        </div>
+		    </span>
 		</section>
 		<section id="about" class="about-me content-section">
 		    <div class="container">
