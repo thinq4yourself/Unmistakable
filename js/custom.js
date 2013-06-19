53ac2a353c04ec315a6a9dfb108c811201a6b1ab
tylergoelz <tyler@tylergoelz.com>
2013-06-19 00:10:36 -0400

    added 2 pixels to the scroll height of the 'check out how' link. Changed the headline-icon position to fixed to have a fixed height and add a parallax effect and get rid of the weird animation and animate as intended

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 $(function() {
 	$('button[data-scroll]').bind('click', function() {
 		$page_location = $(this).attr('data-scroll'),
-		$page_location_top = $page_location ? $($page_location).offset().top : 0;
+		$page_location_top = $page_location ? $($page_location).offset().top + 2 : 0;
 		
 		$('html, body').stop().animate({
 			scrollTop: $page_location_top
