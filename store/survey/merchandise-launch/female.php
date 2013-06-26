2a95a23520dff34ec26c5d2a93995608338ab0d4
tylergoelz <tyler@tylergoelz.com>
2013-06-26 00:57:09 -0400

    added all the files for the merch. Lazy Loading the merch depending on the selection on the first screen. Statically described all the images and linked the correct images

--- store/survey/merchandise-launch/female.php
+++ store/survey/merchandise-launch/female.php
@@ -2,66 +2,66 @@
     <div class="container">
     	<div class="row-fluid merch-row">
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
+    			<img src="/images/store/survey/merchandise-launch/female/white/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
     			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/white/tfotg_desk.png" alt="I Love Front-End Design (white)" />
+    			<p>#TFoTG Desk (white)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/white/tfotg_logo_colorful.png" alt="I Love Front-End Design (white)" />
+    			<p>Colorful Logo (white)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/white/tfotg_logo_solid.png" alt="I Love Front-End Design (white)" />
+    			<p>Solid Logo (white)</p>
     		</div>
     	</div>
     	<div class="row-fluid merch-row">
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/white/what_the_eff.png" alt="I Love Front-End Design (white)" />
+    			<p>What The Eff? (white)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/white/what_the_f-ck.png" alt="I Love Front-End Design (white)" />
+    			<p>What The F*ck? (white)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/white/what_the_fuck.png" alt="I Love Front-End Design (white)" />
+    			<p>What The Fuck? (white)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/silver/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
+    			<p>I <span class="fui-heart"></span> Front-End Design (silver)</p>
     		</div>										
     	</div>
     	<div class="row-fluid merch-row">
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/silver/tfotg_desk.png" alt="I Love Front-End Design (white)" />
+				<p>#TFoTG Desk (silver)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/silver/tfotg_logo_colorful.png" alt="I Love Front-End Design (white)" />
+    			<p>Colorful Logo (silver)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/silver/tfotg_logo_solid.png" alt="I Love Front-End Design (white)" />
+    			<p>Solid Logo (silver)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/silver/what_the_eff.png" alt="I Love Front-End Design (white)" />
+    			<p>What The Eff? (silver)</p>
     		</div>															
     	</div>
     	<div class="row-fluid merch-row">
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/silver/what_the_f-ck.png" alt="I Love Front-End Design (white)" />
+    			<p>What The F*ck? (silver)</p>
     		</div>
     		<div class="span3">
-    			<img src="/images/store/survey/merchandise-launch/i_love_front-end_design.png" alt="I Love Front-End Design (white)" />
-    			<p>I <span class="fui-heart"></span> Front-End Design (white)</p>
+    			<img src="/images/store/survey/merchandise-launch/female/silver/what_the_fuck.png" alt="I Love Front-End Design (white)" />
+    			<p>What The Fuck? (silver)</p>
     		</div>
     	</div>
     </div>
