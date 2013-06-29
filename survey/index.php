60b51cc4da2b3a7d90f5d71d14f70ac512118226
tylergoelz <tyler@tylergoelz.com>
2013-06-29 11:00:00 -0400

    added a survey monkey survey to the site

---
+++ survey/index.php
@@ -0,0 +1,175 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- if page is content page -->
+    <meta property="og:url" content="http://www.tylergoelz.com/survey">
+    <meta property="og:title" content="Tyler Goelz - Applying a Holistic Approach to User Experience">
+    <meta property="og:description" content="">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="http://www.tylergoelz.com/images/logo_social.png">
+
+    <!-- if page is others -->
+    
+    <!-- twitter cards -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@tylergoelz">
+	<meta name="twitter:title" content="Tyler Goelz - Applying a Holistic Approach to User Experience">
+	<meta name="twitter:description" content="Focusing on helping companies put their user's first by making user-centered design & business decisions to enhace the user's experience. Yes, sentence does say &quot;user&quot; three times.">
+	<meta name="twitter:creator" content="@tylergoelz">
+	<meta name="twitter:image:src" content="http://www.tylergoelz.com/images/logo_social.png">
+	<meta name="twitter:domain" content="http://www.tylergoelz.com">
+
+	<meta name="description" content="Focusing on helping companies put their users first by making user-centered design & business decisions to enhace the user's experience. Yes, that sentence does say &quot;user&quot; three times."/>
+	<link rel="canonical" href="http://www.tylergoelz.com" />
+
+	<title>Tyler Goelz - Applying a Holistic Approach to User Experience</title>
+
+  <?php include_once('../includes/head.html'); ?>
+</head>
+
+
+
+<body id="BodyTag" class="notranslate" onload="setup();">
+	<section>
+		<form name="frmS" method="post" action="http://www.surveymonkey.com/s.aspx?sm=KHh7AyJLX3BLXkiqmxhhCQ%3d%3d" id="frmS">
+		    <div>
+		    	<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="/wEWDgL+raDpAgKq7M2aCgLL+IOiBwKg6PF5AqnO0tsHAveko7kIAvm3qN4FAouckuUIArDpqqQBAo3HvMQNAtCN/L0JApzRpLAOAsP2pbEKAtSemZcEs5lKc1Rb2DhjpuYC6hUvzCiqct8=">
+		    	<input type="hidden" name="__VIEWSTATE" id=" __VIEWSTATE" value="">
+		    </div>
+		    <!--content area-->
+		    <div id="PageContentDiv">
+		    	<h1 class="sTitle sTitleCf">
+		    	<div style="text-align:left;">
+		    		<span class="notranslate">A Quick, 3 Question #TFoTG Survey<br>
+		    		</span>
+		    	</div>
+		    	</h1>
+		    	<div class="pTitle">
+		    		<h2></h2>
+		    		&nbsp;<br class="clear">
+		    	</div>
+		    	<div class="pDesc">
+		    		I hope you're enjoying your time on #TFoTG. I'm always looking for ways to improve so I'd love your feedback.
+		    	</div>
+		    	<div class="pgHdr">
+		    		<div id="q1" class="question" style="margin:0 0 0 0;width:auto">
+		    			<div class="qContent">
+		    				<div class="qHeader">
+		    					I Like...
+		    				</div>
+		    				<div class="qBody">
+		    					<span><textarea id="text_539451027_0" name="text_539451027_0" class="open" rows="3" cols="50"></textarea></span>
+		    				</div>
+		    			</div>
+		    		</div>
+		    		<div id="q2" class="question" style="margin:0 0 0 0;width:auto">
+		    			<div class="qContent">
+		    				<div class="qHeader">
+		    					I Wish...
+		    				</div>
+		    				<div class="qBody">
+		    					<span><textarea id="text_540687523_0" name="text_540687523_0" class="open" rows="3" cols="50"></textarea></span>
+		    				</div>
+		    			</div>
+		    		</div>
+		    		<div id="q3" class="question" style="margin:0 0 0 0;width:auto">
+		    			<div class="qContent">
+		    				<div class="qHeader">
+		    					What if...
+		    				</div>
+		    				<div class="qBody">
+		    					<span><textarea id="text_540687553_0" name="text_540687553_0" class="open" rows="3" cols="50"></textarea></span>
+		    				</div>
+		    			</div>
+		    		</div>
+		    		<div id="q4" class="question" style="margin:0 0 0 0;width:auto">
+		    			<div class="qContent">
+		    				<div class="qHeader">
+		    					Name (Totally Optional)
+		    				</div>
+		    				<div class="qBody">
+		    					<table cellspacing="0" cellpadding="3" border="0" style="width:100%;">
+		    					<tbody>
+		    					<tr>
+		    						<td style="width:20%;">
+		    							<label for="text_540687632_6288986057">First</label>
+		    						</td>
+		    						<td style="width:80%;padding-top:3px;" valign="top">
+		    							<input id="text_540687632_6288986057" name="text_540687632_6288986057" class="open" type="text" size="50" value="">
+		    						</td>
+		    					</tr>
+		    					<tr>
+		    						<td style="width:20%;">
+		    							<label for="text_540687632_6288986058">Last</label>
+		    						</td>
+		    						<td style="width:80%;padding-top:3px;" valign="top">
+		    							<input id="text_540687632_6288986058" name="text_540687632_6288986058" class="open" type="text" size="50" value="">
+		    						</td>
+		    					</tr>
+		    					</tbody>
+		    					</table>
+		    				</div>
+		    			</div>
+		    		</div>
+		    		<div id="q5" class="question" style="margin:0 0 0 0;width:auto">
+		    			<div class="qContent">
+		    				<div class="qHeader">
+		    					<abbr class="noborder" title="Question 5">5</abbr>. Email (If you want a reply)
+		    				</div>
+		    				<div class="qBody">
+		    					<span><input id="text_540687645_0" name="text_540687645_0" class="open" type="text" size="50" value=""></span>
+		    				</div>
+		    			</div>
+		    		</div>
+		    	</div>
+		    </div>
+		    <!--end content area-->
+		    <div id="panButtonBar">
+		    	<div style="text-align:center;">
+		    		<input type="submit" name="NextButton" value="Done" onclick="onesubmit(this);" id="NextButton" class="btn btntext grey">
+		    		<!---->
+		    	</div>
+		    </div>
+		    <input type="hidden" name="hid_smC0l1d" id="hid_smC0l1d" value="pgI3hDfUOYXkyQHyYjOwpg_3d_3d">
+		    <input type="hidden" name="hid_smRsL1d" id="hid_smRsL1d">
+		    <input type="hidden" name="hid_smRs1d" id="hid_smRs1d" value="E6uK1MhOcpBUysyKlC0vrg_3d_3d">
+		    <input type="hidden" name="hid_smCSV" id="hid_smCSV">
+		    <input type="hidden" name="hid_smS1d" id="hid_smS1d" value="u5IyCeVFEPGOS7BGnoI_2fVA_3d_3d">
+		    <input type="hidden" name="hid_smM0D" id="hid_smM0D" value="E6uK1MhOcpBUysyKlC0vrg_3d_3d">
+		    <input type="hidden" name="hid_smV3Rsn" id="hid_smV3Rsn" value="4D_2ffGjLNp1kpVfY8S7qdJw_3d_3d">
+		    <input type="hidden" name="hid_smS3CT1d" id="hid_smS3CT1d" value="zCAwDwhqiKOI_2fg_2fTvRKNYg_3d_3d">
+		    <input type="hidden" name="hid_DC" id="hid_DC" value="w3gTRb3Jb7FpH02hmLD6tSmXHhZIUf0dFCQYnaXH_2f_2bP23v00QHTc_2fk4Cqm9i3b_2fr">
+		    <input type="hidden" name="Hidden_CollectorToken" id="Hidden_CollectorToken">
+		    <input type="hidden" name="Hidden_Simple" id="Hidden_Simple">
+		    <input type="hidden" name="hid_l04dez" id="hid_l04dez" value="TehvMQFhTWZfRotKwDjuce_2fzbrxgbRBIT0_2bO_2f7SbprI_3d">
+		    <div>
+		    </div>
+		</form>
+	</section>
+	<footer>
+		<div class="pbf">
+		    	 Powered by <a href="http://www.surveymonkey.com/"><span>SurveyMonkey</span></a>
+		    	<br>
+		    	<div id="SampleSurveysFooter">
+		    		 Check out our <a id="pbhl" title="Go to SurveyMonkey.com to create free online surveys" href="http://www.surveymonkey.com/mp/survey-templates/" target="_blank">sample surveys</a> and create your own now!
+		    	</div>
+		    </div>
+		    <div class="pbf">
+		    	<a id="pbhli" href="http://www.surveymonkey.com/?ut_source=survey_poweredby&amp;utm_medium=AvL3PdLureuTOeLUcDAWf%2fcVY5wiTkX47OY1MoZjjZY%3d&amp;utm_term=SurveyBasic&amp;utm_campaign=ysrUvWawcIvdCXFlpN%2f2zw%3d%3d&amp;utm_content=1" target="_blank"></a>
+		    </div>
+	</footer>
+<?php include_once('../includes/scripts.html'); ?>
+<!-- Survey Monkey Code -->
+<noscript>
+&lt;style type="text/css" media="all"&gt;form {display:none;} #jserror {text-align:center;margin-top:50px;}&lt;/style&gt;&lt;div id="jserror" class="qHeader"&gt;Javascript is required for this site to function, please enable.&lt;/div&gt;
+</noscript>
+<script type="text/javascript">
+/* <![CDATA[ */
+function nes(e){e=(e)?e:event;var c=(e.which)?e.which:e.keyCode;return(c!=13);}var is=document.getElementsByTagName("input");for(var i=0;i<is.length;i++){var _i=is[i];if(_i.type=="text")_i.onkeypress=function(e){ return nes(e); };}
+/* ]]> */
+</script>
+<!-- Survey Monkey Code -->
+</body>
+</html>
