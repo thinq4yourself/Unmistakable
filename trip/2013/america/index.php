4028adafe6c561a534d527dfab3f3b8751968b2a
tylergoelz <tyler@tylergoelz.com>
2013-11-08 13:26:13 -0500

    added part two of Vegas link to the trip page

--- trip/2013/america/index.php
+++ trip/2013/america/index.php
@@ -108,8 +108,8 @@
 							<small class="muted">Posted: 10/11/2013</small>
 							<p>Along the way, I heard a few different reactions to the cross country road trip I took from St. Petersburg, Florida to Tacoma, Washington ranging from “It must be nice to be rich” to “you must have saved up for a long time”, neither of which were true. I’m going to share with you how I was able to travel 4900 miles over a four-week span, visiting 18 cities in 10 different states.</p>
 							<p>About 8 months ago, I read a book called <a href="http://www.fourhourworkweek.com/" title="The 4-Hour Work Week">The 4-Hour Work Week</a> (4HWW) by <a href="http://www.fourhourworkweek.com/blog" title="Tim Ferris">Tim Ferris</a>. The book focuses on lifestyle design and uses a formula to be able to live the life that you want. The formula can be summed up in the following acronym: DEAL. Definition, Elimination, Automation and Liberation. Here’s how I used Definition, Elimination and Liberation to make this road trip happen and how I discovered an idea for a muse (Automation) along the way.</p>
-							<button type="button" data-scroll="#tallahassee" class="btn btn-large btn-primary">Read More About My Trip</button>
-							<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/63787007406/a-beard-across-america-my-first-successfull-dreamline">Continue Reading This Article &#8594;</a>
+							<button type="button" data-scroll="#tallahassee" class="btn btn-large">Read More About My Trip &#8595;</button>
+							<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/63787007406/a-beard-across-america-my-first-successfull-dreamline">Continue Reading This Article &#8594;</a>
 						</div>
 					</div>
 				</div>
@@ -127,8 +127,8 @@
 								<small class="muted">Visited: 9/9/2013 (1 night)</small>
 								<small class="muted">Posted: 10/18/2013</small>
 								<small>I was excited to reach Tallahassee since this would be the first time I had ever <a href="http://www.couchsurfing.org" title="CouchSurfing">CouchSurf</a>’d. I was staying with <a href="https://www.couchsurfing.org/people/strangepowers/" title="Chris and Meagan (StrangePowers)">Chris & Meagan</a> and wasn’t quite sure what to expect. Most likely a cool couple offering a coach for me to rest my head on and an hour or two of good conversation. My only concern was that they’d be unhappy with the fact that my arrival time was a few hours later than I expected, but with open communication between Chris and I along the way, things worked out just fine.</small>
-								<button type="button" data-scroll="#nola" class="btn btn-large btn-primary">Read More About My Trip</button>
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/64410518374/tallahassee-florida-big-ass-pizza-and-freedom-salute-ame">Continue Reading This Article &#8594;</a>
+								<button type="button" data-scroll="#nola" class="btn btn-large">Read More About My Trip &#8595;</button>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/64410518374/tallahassee-florida-big-ass-pizza-and-freedom-salute-ame">Continue Reading This Article &#8594;</a>
 							</span>
 						</div>
 					</div>
@@ -146,8 +146,8 @@
 								<small class="muted">Posted: 10/21/2013</small>
 								<small>I was excited to reach New Orleans for multiple reasons; It's New Orleans, it was my first time in a hostel and I had a list of recommended places to visit from my father and people that I've encountered that are from Nola.</small>
 								<small>As I pulled into town, my first stop was to check into the <a href="http://www.indiahousehostel.com/" title="India House Hostel">India House Hostel</a> which is located on Canal and South Lopez Street. It was a series of houses that were purchased shortly after Katrina and converted into a hostel. The atmosphere was fantastically eclectic and welcoming.</small>
-								<button type="button" data-scroll="#houston" class="btn btn-large btn-primary">Read More About My Trip</button>
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/64712353667/new-orleans-louisiana-india-house-hostel-and-frenchmen-s">Continue Reading This Article &#8594;</a>
+								<button type="button" data-scroll="#houston" class="btn btn-large">Read More About My Trip &#8595;</button>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/64712353667/new-orleans-louisiana-india-house-hostel-and-frenchmen-s">Continue Reading This Article &#8594;</a>
 							</span>
 						</div>
 					</div>
@@ -164,8 +164,8 @@
 								<small class="muted">Visited: 9/12/2013</small>
 								<small class="muted">Posted: 10/23/2013</small>
 								<small>As I departed <a href="http://blog.tylergoelz.com/post/64712353667/new-orleans-louisiana-india-house-hostel-and-frenchmen-s" title="New Orleans">New Orleans</a>, I had absolutely no idea of what to expect from Texas. It would be the furthest I had ever traveled west and in all honesty, I wasn&rsquo;t even sure of the geographical order of the cities I would be visiting in Texas. For example, I had told my father as I was leaving Nola that I&rsquo;d be in Austin that night. That was not the case. After looking at my itinerary, I was going to <a href="https://www.couchsurfing.org/" title="CouchSurfing">CouchSurf</a> with a guy named <a href="http://www.couchsurfing.org/people/abhijit.mohod/" title="Abhijit">Abhijit</a>.</small>
-								<button type="button" data-scroll="#austin" class="btn btn-large btn-primary">Read More About My Trip</button>
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/64810203129/houston-texas-hidden-underground-tunnels-and-the-beginni">Continue Reading This Article &#8594;</a>
+								<button type="button" data-scroll="#austin" class="btn btn-large">Read More About My Trip &#8595;</button>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/64810203129/houston-texas-hidden-underground-tunnels-and-the-beginni">Continue Reading This Article &#8594;</a>
 							</span>
 						</div>
 					</div>
@@ -182,8 +182,8 @@
 								<small class="muted">Visited: 9/13/2013 (1 night)</small>
 								<small class="muted">Posted: 10/25/2013</small>
 								<small>I needed to find an alternative to <a href="https://www.couchsurfing.org/" title="CouchSurfing">CouchSurfing</a> for lodging in Austin. My search for a host had left me without a place to stay. I was searching for hostels until <a href="http://www.couchsurfing.org/people/abhijit.mohod/">Abhi</a>, my CouchSurfing host in <a href="http://blog.tylergoelz.com/post/64810203129/houston-texas-hidden-underground-tunnels-and-the-beginni">Houston</a>, mentioned a website called <a href="https://www.airbnb.com/">AirBnB</a>. I had never heard of the website before, but was up for the new experience. AirBnB is basically the same concept of CouchSurfing, but with cost associated with your stay. I found a place to stay that was only a few blocks from <a href="http://en.wikipedia.org/wiki/Sixth_Street_(Austin,_Texas)">East 6th Street</a> with a guy named <a href="https://www.airbnb.com/rooms/70342">Nick</a> and his roommates.</small>
-								<button type="button" data-scroll="#fort-worth" class="btn btn-large btn-primary">Read More About My Trip</button>
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/65060797407/austin-texas-hootin-n-hollerin-lone-star-beer-and-mermai">Continue Reading This Article &#8594;</a>
+								<button type="button" data-scroll="#fort-worth" class="btn btn-large">Read More About My Trip &#8595;</button>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/65060797407/austin-texas-hootin-n-hollerin-lone-star-beer-and-mermai">Continue Reading This Article &#8594;</a>
 							</span>
 						</div>
 					</div>
@@ -200,8 +200,8 @@
 								<small class="muted">Visited: 9/14/2013 (1 night)</small>
 								<small class="muted">Posted: 10/28/2013</small>
 								<small>I originally planned to stay in Dallas for my last night in Texas, since I wanted to visit "The Big 3" cities I had heard of. I kept hearing the same things about Dallas from the Texans I stayed with, "It's pretentious", "It's like the L.A. of Texas", "There are a lot of $30k millionaire douche bags". Fortunately, I received a message on <a href="https://www.couchsurfing.org/" title="CouchSurfing">CouchSurfing</a> from <a href="https://www.couchsurfing.org/people/bradwilsonem/" title="Brad">Brad</a> who was offering an extra room in his apartment. He mentioned it would be a great weekend to visit since him and his wife, <a href="https://www.couchsurfing.org/people/marybethwilson/" title="Marybeth">Marybeth</a> would be brewing beer. I was sold.</small>
-								<button type="button" data-scroll="#oklahoma" class="btn btn-large btn-primary">Read More About My Trip</button>
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/65362469181/fort-worth-texas-breakfast-beer-hemphill-brew-pub-pizza">Continue Reading This Article &#8594;</a>
+								<button type="button" data-scroll="#oklahoma" class="btn btn-large">Read More About My Trip &#8595;</button>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/65362469181/fort-worth-texas-breakfast-beer-hemphill-brew-pub-pizza">Continue Reading This Article &#8594;</a>
 							</span>
 						</div>
 					</div>
@@ -218,8 +218,8 @@
 								<small class="muted">Visited: 9/15/2013 (1 night)</small>
 								<small class="muted">Posted: 10/30/2013</small>
 								<small>By this point, I was quite comfortable with the way <a href="https://www.couchsurfing.org/" title="CouchSurfing">CouchSurfing</a> works. You connect with a local from a city you will be visiting, meet them when you arrive, grab dinner & a few drink and enjoy the city like the locals do. This was not the case in Oklahoma City.</small>
-								<button type="button" data-scroll="#kansas" class="btn btn-large btn-primary">Read More About My Trip</button>
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/65529531234/oklahoma-city-oklahoma-is-anybody-home">Continue Reading This Article &#8594;</a>
+								<button type="button" data-scroll="#kansas" class="btn btn-large">Read More About My Trip &#8595;</button>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/65529531234/oklahoma-city-oklahoma-is-anybody-home">Continue Reading This Article &#8594;</a>
 							</span>
 						</div>
 					</div>
@@ -236,8 +236,8 @@
 								<small class="muted">Visited: 9/16/2013 (1 night)</small>
 								<small class="muted">Posted: 11/01/2013</small>
 								<small>Up until this point, I had only stayed with one couple and besides that, mostly bachelor men and transient travelers. Wichita was different since I would be <a href="https://www.couchsurfing.org/" title="CouchSurfing">CouchSurfing</a> with <a href="https://www.couchsurfing.org/people/edbrot/" title="Eddy">Eddy</a>, his wife Rema and his daughter Norah whose fourth birthday landed on the same day I would be in Wichita. I received an eager message from Eddy about a week before my stay talking about his passion for technology, good food and a decent beer selection; all things good. I was instantly excited to meet him and his family.</small>
-								<button type="button" data-scroll="#denver" class="btn btn-large btn-primary">Read More About My Trip</button>
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/65691982794/wichita-kansas-family-friends-homemade-pizza-and">Continue Reading This Article &#8594;</a>
+								<button type="button" data-scroll="#denver" class="btn btn-large">Read More About My Trip &#8595;</button>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/65691982794/wichita-kansas-family-friends-homemade-pizza-and">Continue Reading This Article &#8594;</a>
 							</span>
 						</div>
 					</div>
@@ -254,8 +254,8 @@
 								<small class="muted">Visited: 9/17/2013 (2 nights)</small>
 								<small class="muted">Posted: 11/04/2013</small>
 								<small>I was so excited to go to Denver, since my girlfriend, Kimmi, met me there and joined me for nine days of the trip, visiting her mother & sister in Denver and driving with me through Las Vegas, Napa Valley and San Francisco. I woke up and left <a href="http://blog.tylergoelz.com/post/65691982794/wichita-kansas-family-friends-homemade-pizza-and" title="Wichita, Kansas">Wichita, Kansas</a> extra early in preparation for the long drive ahead of me.</small>
-								<button type="button" data-scroll="#nevada" class="btn btn-large btn-primary">Read More About My Trip</button>
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/66043300759/denver-colorado-the-girl-the-pioneer-and-red-rocks-amphi">Continue Reading This Article &#8594;</a>
+								<button type="button" data-scroll="#nevada" class="btn btn-large">Read More About My Trip &#8595;</button>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/66043300759/denver-colorado-the-girl-the-pioneer-and-red-rocks-amphi">Continue Reading This Article &#8594;</a>
 							</span>
 						</div>
 					</div>
@@ -272,8 +272,9 @@
 								<small class="muted">Visited: 9/19/2013 (3 nights)</small>
 								<small class="muted">Posted: 11/06/2013</small>
 								<small>Ever since Kimmi, my girlfriend, went to Vegas when she was 21, she had always told me she would love to go back. Who wouldn’t love to go to Vegas?! I know I was super excited about it, especially since it was my first time in Sin City. To give you an idea of how stoked we were, we drove straight from Denver to Las Vegas totally skipping our layover town in Utah. For any of you that haven’t driven from Denver to Vegas, it takes about eleven hours IF you don’t stop, but it was decided, the adrenaline had taken over and we weren’t stopping until we saw Vegas, baby!</small>
-								<!-- <button type="button" data-scroll="#paso-robles" class="btn btn-large btn-primary">Read More About My Trip</button> -->
-								<a type="button" class="btn btn-large" href="http://blog.tylergoelz.com/post/66243902496/las-vegas-nevada-part-1-raphael-rivera-canadians-new-yor">Continue Reading This Article &#8594;</a>
+								<!-- <button type="button" data-scroll="#paso-robles" class="btn btn-large">Read More About My Trip &#8595;</button> -->
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/66243902496/las-vegas-nevada-part-1-raphael-rivera-canadians-new-yor">Read Part 1 &#8594;</a>
+								<a type="button" class="btn btn-large btn-primary" href="http://blog.tylergoelz.com/post/66383110063/las-vegas-nevada-part-2-big-buck-hunter-the-big-ass-whee">Read Part 2 &#8594;</a>
 							</span>
 						</div>
 					</div>
