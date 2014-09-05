751f42fbdea96510cd8b191ab9043e1b01bf0204
tylergoelz <tyler@tylergoelz.com>
2014-09-05 12:44:28 -0400

    moved the legacy site to a sub folder and imported a fresh version of the new flat-ui (free) to build the updated version of the site using updated tech.

---
+++ legacy/index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- if page is content page -->
+	<meta property="og:url" content="http://www.tylergoelz.com">
+	<meta property="og:title" content="Tyler Goelz - Applying a Holistic Approach to User Experience">
+	<meta property="og:description" content="Focusing on helping companies put their users first by making user-centered design & business decisions to enhace the user's experience. Yes, that sentence does say &quot;user&quot; three times.">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="http://www.tylergoelz.com/images/logo_social.png">
+
+	<!-- if page is others -->
+
+	<!-- twitter cards -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@tylergoelz">
+	<meta name="twitter:title" content="Tyler Goelz - Applying a Holistic Approach to User Experience">
+	<meta name="twitter:description" content="Focusing on helping companies put their user's first by making user-centered design & business decisions to enhace the user's experience. Yes, sentence does say &quot;user&quot; three times.">
+	<meta name="twitter:creator" content="@tylergoelz">
+	<meta name="twitter:image:src" content="http://www.tylergoelz.com/images/logo_social.png">
+	<meta name="twitter:domain" content="http://www.tylergoelz.com">
+	<meta property="twitter:account_id" content="15271634" />
+
+	<meta name="description" content="Focusing on helping companies put their users first by making user-centered design & business decisions to enhace the user's experience. Yes, that sentence does say &quot;user&quot; three times."/>
+	<link rel="canonical" href="http://www.tylergoelz.com" />
+
+	<title>Tyler Goelz - Applying a Holistic Approach to User Experience</title>
+
+	<?php include_once($_SERVER["DOCUMENT_ROOT"] . '/includes/head.html'); ?>
+</head>
+<body id="homepage" class="homepage">
+	<article>
+		<?php include_once($_SERVER["DOCUMENT_ROOT"] . '/includes/follow-me-widget.html'); ?>
+		<section id="headline" class="headline">
+			<div id="headline-icons" class="headline-icons">
+				<div class="moleskin"><img src="/images/headline/moleskin.png" alt="moleskin" /></div>
+				<div class="coffee"><img src="/images/headline/coffee.png" alt="coffee" /></div>
+				<div class="sticky"><img src="/images/headline/sticky.png" alt="sticky" /></div>
+				<div class="s3"><img src="/images/headline/s3.png" alt="s3" /></div>
+				<div class="keyboard"><img src="/images/headline/keyboard.png" alt="keyboard" /></div>
+			</div>
+			<div class="info">
+				<h1>Tyler Goelz</h1>
+				<small>Applying a Holistic Approach to User Experience</small>
+				<button type="button" data-scroll="#approach" class="btn btn-large btn-primary">Check Out How</button>
+			</div>
+		</section>
+		
+		<section id="approach" class="approach-section">
+			<div class="container">
+				<div class="row">
+					<h1>My Approach</h1>
+					<div class="span6 topic">
+						<div class="topic-image">
+							<img src="images/icons/Clipboard@2x.png" alt="Clipboard">
+						</div>
+						<div class="frame">
+							<h3>Customer Validation</h3>
+							<p>Envision the customer, conduct quality interviews and learn from customer development.</p>
+						</div>
+					</div>
+					<div class="span6 topic">
+						<div class="topic-image">
+							<img src="images/icons/Bullseye@2x.png" alt="Clipboard">
+						</div>
+						<div class="frame">
+							<h3>Product Market Fit</h3>
+							<p>Use a process of design, distribution and data collection to find messaging, pricing & audience.</p>
+						</div>
+					</div>
+					<div class="span6 topic">
+						<div class="topic-image">
+							<img src="images/icons/Tools@2x.png" alt="Clipboard">
+						</div>
+						<div class="frame">
+							<h3>Product Design</h3>
+							<p>Deliver products through Lean UX - a combo of <a href="http://en.wikipedia.org/wiki/Lean_Startup#Description?ref=tylergoelz" target="_blank">Lean Startup</a> and User Experience.</p>
+						</div>		    			
+					</div>
+					<div class="span6 topic">
+						<div class="topic-image">
+							<img src="images/icons/Touch@2x.png" alt="Clipboard">
+						</div>
+						<div class="frame">
+							<h3>User Testing</h3>
+							<p>Discover opportunities that could serve user’s needs and inform the rest of the design process.</p>
+						</div>		    			
+					</div>
+					<div class="span6 topic">
+						<div class="topic-image">
+							<img src="images/icons/Chart@2x.png" alt="Clipboard">
+						</div>
+						<div class="frame">
+							<h3>Scalability</h3>
+							<p>Start, design, optimize and scale products and business models.</p>
+						</div>		    			
+					</div>
+					<div class="span6 topic">
+						<div class="topic-image">
+							<img src="images/icons/Baggage@2x.png" alt="Clipboard">
+						</div>
+						<div class="frame">
+							<h3>Lifestyle Design</h3>
+							<p>“Life doesn’t have to be so damn hard. It really doesn’t.” - <a href="http://www.fourhourworkweek.com/blog/?ref=tylergoelz" target="_blank">Tim Ferriss</a></p>
+						</div>		    			
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="muse" class="muse-section">
+			<div class="muse">
+				<span class="details">
+					<span class="frame">
+						<h2>Redwood Agile</h2>
+						<p>Redwood Agile is my current Muse*. We apply everything mentioned above to help entrepreneurs start, design, optimize and scale their products and business models.</p>
+					</span>
+				</span>
+				<span class="definition">
+					<span class="frame">
+						<p><small>*Muse /myooz/ (noun)</small></p>
+						<p><small>A low-maintenance business that generates significant income.</small></p>
+					</span>
+				</span>
+				<div class="web">
+					<a href="http://www.redwoodagile.com/?ref=tylergoelz" target="_blank" class="web-block">
+						<img src="/images/muse/eye-icon.png" class="icon">
+						<span class="web-element1">
+							<img src="/images/muse/redwood/page-element-1.png" alt="element">
+						</span>
+						<span class="web-element3">
+							<img src="/images/muse/redwood/page-element-3.png" alt="element">
+						</span>
+						<span class="web-element2">
+							<img src="/images/muse/redwood/page-element-2.png" alt="element">
+						</span>
+					</a>
+				</div>
+
+				<div class="buttonholder">
+					<a href="http://www.redwoodagile.com/?ref=tylergoelz" class="btn btn-primary btn-large" target="_blank"><span class="fui-eye"></span>&nbsp;View Redwood Agile</a>
+				</div>
+
+			</div>
+		</section>
+		<section id="about" class="about-me-section">
+			<div class="about">
+				<img id="icon-face" src="/images/about/logo.png" width="398" alt="The Face of Tyler Goelz">
+				<span class="frame">
+					<h2>About Me</h2>
+					<p>Traveler, writer, and craft beer drinker. Author of <a href="www.abeardacrossamerica.com">A Beard Across America</a> and Co-Founder of <a href="https://twitter.com/sproutcamp">SproutCamp</a>. Helping world changers <a href="https://twitter.com/beunmistakable">#BeUnmistakable.</a></p>
+					<p>Along with writing, traveling and creating muses, I focus on helping companies succeed by putting their users first. I help them do that by making user-centered design & business decision to enhance the user's experience.</p>
+					<p>Follow me:
+						<a class="follow-link" href="http://www.abeardacrossamerica.com/">#ABAA</a>
+						<a class="follow-link" href="http://medium.com/@tylergoelz">Blog</a>
+						<a class="follow-link" href="http://www.twitter.com/tylergoelz">Twitter</a>
+						<a class="follow-link" href="http://www.facebook.com/ILikeTylerGoelz">Facebook</a>
+					</p>
+				</span>
+			</div>  	
+		</section>
+		<section id="social" class="share-section">
+			<div class="container">
+				<div class="row">
+					<h1>Sharing is Caring!</h1>
+					<ul class="share-list">
+						<li><a href="javascript:oiw('https://twitter.com/intent/tweet?hashtags=abaa&original_referer=http%3A%2F%2Fwww.tylergoelz.com%2F&text=Tyler%20Goelz%20-%20Applying%20a%20Holistic%20Approach%20to%20User%20Experience&tw_p=tweetbutton&url=http%3A%2F%2Fwww.tylergoelz.com','',695,300)" class="twitter"><dt>Tweet</dt><span></span></a></li>
+						<li><a href="javascript:oiw('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Ftylergoelz.com','',661,338)" class="facebook"><dt>Share</dt><span></span></a></li>
+						<li><a href="javascript:oiw('https://plus.google.com/share?url=http%3A%2F%2Fwww.tylergoelz.com','',666,378)" class="google"><dt>Share</dt><span></span></a></li>
+						<li><a href="javascript:oiw('http://pinterest.com/pin/create/button/?url=http://tylergoelz.com/&media=http://tylergoelz.com/images/about/logo.png&description=Tyler%20Goelz%20-%20Applying%20a%20Holistic%20Approach%20to%20User%20Experience','',630,360);" class="pin"><dt>Pin it</dt><span></span></a></li>
+						<li><a href="javascript:oiw('http://www.linkedin.com/shareArticle?mini=true&url=http://tylergoelz.com&title=Tyler%20Goelz&summary=Applying%20a%20Holistic%20Approach%20to%20User%20Experience','',520,385)" class="linkedin"><dt>Share</dt><span></span></a></li>
+					</ul>
+				</div>
+			</div>			
+		</section>
+		<span class="share-section-cont"> </span>
+	</article>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="references">
+					<p>This site was crafted using the <a href="http://designmodo.com/flat-free/?ref=tylergoelz" target="_blank">Flat UI</a> framework, mocked up in <a href="http://www.adobe.com/products/photoshop.html?ref=tylergoelz" target="_blank">Photoshop</a> & <a href="http://www.adobe.com/products/illustrator.html?ref=tylergoelz" target="_blank">Illustrator</a> CS6, build in <a href="http://www.sublimetext.com/2?ref=tylergoelz" target="_blank">Submlime Text 2</a>, using <a href="http://en.wikipedia.org/wiki/HTML5?ref=tylergoelz" target="_blank">HTML5</a>, <a href="https://en.wikipedia.org/wiki/Cascading_Style_Sheets#CSS_3?ref=tylergoelz" target="_blank">CS3</a> & <a href="http://jquery.com/?ref=tylergoelz" target="_blank">jQuery</a>. It's stashed using <a href="https://devcenter.heroku.com/articles/git/?ref=tylergoelz" target="_blank">git</a> and hosted on <a href="https://www.heroku.com/?ref=tylergoelz" target="_blank">Heroku</a>.</p>
+					<p>Thank you all for your community support and general badassness!</p>
+					<small>&copy; 2013 The Face of Tyler Goelz, Inc.</small>
+				</div>
+			</div>
+		</div>
+	</footer>   
+	<?php include_once($_SERVER["DOCUMENT_ROOT"] . '/includes/scripts.html'); ?>
+</body>
+</html>
