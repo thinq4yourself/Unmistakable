128d33b033b6586d3b76a806b77534e085addc71
tylergoelz <tyler@tylergoelz.com>
2014-09-05 15:00:34 -0400

    added a navigation and nav items

--- index.php
+++ index.php
@@ -8,6 +8,48 @@
   <?php include( $_SERVER['DOCUMENT_ROOT'].'/includes/head.php'); ?>
 </head>
 <body>
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Brand</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Approach</a></li>
+          <li><a href="#">Muses</a></li>
+          <li><a href="#">About</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Share the Love <i class="fui-heart"></i><i class="caret"></i></a>
+            <ul class="dropdown-menu" role="menu">
+              <li rel="0"><a tabindex="-1" href="http://medium.com/@tylergoelz">Blog</a></li>
+              <li rel="1"><a tabindex="-1" href="http://www.twitter.com/tylergoelz">Twitter</a></li>
+              <li rel="2"><a tabindex="-1" href="http://www.facebook.com/ILikeTylerGoelz">Facebook</a></li>
+              <li rel="3"><a tabindex="-1" href="https://plus.google.com/+TylerGoelz">Google+</a></li>
+              <li rel="4"><a tabindex="-1" href="http://www.linkedin.com/in/tylergoelz/">Linkedin</a></li>
+              <li rel="5"><a tabindex="-1" href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/survey">Give Feedback</a> </li>
+            </ul>
+          </li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-->
+  </nav>
+  <div class="jumbotron">
+    <div class="container super-hero">
+      <h1>Tyler Goelz
+        <small>Helping people to find and live their passion <br class="visible-lg" /> while making the world a better place through technology.</small>
+      </h1>
+    </div>
+  </div>
 
   <div class="super-hero">
     <h1 class="demo-logo">
