896c5ac6349e97012cf4ac55c194727909e236f4
tylergoelz <tyler@tylergoelz.com>
2014-09-10 14:40:11 -0400

    added padding to take into consideration the  height of the navigatio and removing it from how far down the page will scrolln

--- bearded-beta/js/custom.scrollto.js
+++ bearded-beta/js/custom.scrollto.js
@@ -1,13 +1,15 @@
 $(function() {
 	$('[data-scroll]').bind('click', function(e) {
 		e.preventDefault();
-		$page_location = $(this).attr('data-scroll');
+		var $page_location = $(this).attr('data-scroll');
 		scrollToSection($page_location, 1000);
 	});
 });
 
 function scrollToSection($page_location, duration) {
-	$page_location_top = $page_location ? $($page_location).offset().top + 2 : 0;
+	var $padding = $('nav.navbar-fixed-top .navbar-header').height(),
+	$page_location_top = $page_location ? $($page_location).offset().top - $padding : 0,
+	duration = duration ? duration : 1000;
 
 	$('html, body').stop().animate({
 		scrollTop: $page_location_top
