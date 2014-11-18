e8dc643b6be6a4e02052033e0b5dbee837c8474c
yaboi <tyler@tylergoelz.com>
2014-11-18 03:03:10 +0000

    Add heroku toolbelt, bump up dynos, and updated web.js file to try and fix heroku

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var morgan = require('morgan');
 var app = express();
-
-app.use(morgan('app'));
+ 
+app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
