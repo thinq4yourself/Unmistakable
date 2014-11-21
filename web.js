b8e80dd8fb2aed7c7e89ff1b629d4c777ed31543
thinq4yourself <joel@thinq4yourself.com>
2014-11-21 21:32:41 +0000

    commented out force SSL in express until cloudflare ssl cert is ready on nov 22)

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+/* uncomment this when the cloudfare SSL provisioning is complete on November 22
 app.use(function(req, res, next) {
     if (req.headers['x-forwarded-proto'] != 'https') {
         res.redirect('https://' + req.headers.host + req.path);
@@ -10,6 +11,7 @@ app.use(function(req, res, next) {
         return next();
     }
 });
+*/
 app.use(morgan('app'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
