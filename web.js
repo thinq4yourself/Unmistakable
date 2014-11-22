1201c1825afce81b59eef542f5007ceb68a27363
thinq4yourself <joel@thinq4yourself.com>
2014-11-22 05:41:36 +0000

    in web.js, we are nore forcing SSL. Yay.

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
-/* uncomment this when the cloudfare SSL provisioning is complete on November 22
 app.use(function(req, res, next) {
     if (req.headers['x-forwarded-proto'] != 'https') {
         res.redirect('https://' + req.headers.host + req.path);
@@ -11,7 +10,6 @@ app.use(function(req, res, next) {
         return next();
     }
 });
-*/
 app.use(morgan('app'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
