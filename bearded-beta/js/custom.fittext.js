1f637a72b38f8898b9630750a8306106f4a79f76
yaboi <tyler@tylergoelz.com>
2014-12-05 00:32:49 +0000

    Remove /bearded-beta in order to use staging site as intended.. to stage

--- bearded-beta/js/custom.fittext.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$("h1.headline").fitText(.85, { minFontSize: '32px', maxFontSize: '90px' });
-	$("h2.headline").fitText(.90, { minFontSize: '28px', maxFontSize: '72px' });
-	$("h3.headline").fitText(.95, { minFontSize: '24px', maxFontSize: '54px' });
-	$("h4.headline").fitText(1, { minFontSize: '20px', maxFontSize: '48px' });
-});
