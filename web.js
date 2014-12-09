b8e24f93cd065cefdc2b1282d7ad008503446e59
yaboi <tyler@tylergoelz.com>
2014-12-09 02:28:22 +0000

    Remove force https in web.js

--- web.js
+++ web.js
@@ -4,14 +4,14 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
-app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] != 'https') {
-        res.redirect('https://' + req.headers.host + req.path);
-    }
-    else {
-        return next();
-    }
-});
+// app.use(function(req, res, next) {
+//     if (req.headers['x-forwarded-proto'] != 'https') {
+//         res.redirect('https://' + req.headers.host + req.path);
+//     }
+//     else {
+//         return next();
+//     }
+// });
 app.use(morgan('app'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
