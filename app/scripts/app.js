87e8536569f31e6442ddd15162403e494d0909c3
yaboi <tyler@tylergoelz.com>
2014-12-13 19:44:21 +0000

    Remove newrelic.js file, again.

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+/**
+ * @ngdoc overview
+ * @name passionDrivenPortfolio
+ * @description
+ * # passionDrivenPortfolio
+ *
+ * Main module of the application.
+ */
+angular.module('passionDrivenPortfolio', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    
+    'ui.bootstrap',
+    'ngFitText',
+
+    'app.controllers'
+
+]).config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/main.html'
+    }).when('/traveler', {
+        templateUrl: 'views/traveler.html'
+    }).when('/developer', {
+        templateUrl: 'views/developer.html'
+    }).when('/404/', {
+        templateUrl: 'views/404.html'
+    }).otherwise({
+        redirectTo: '404'
+    });
+    
+    $locationProvider.html5Mode(true);
+});
