e40e069a2e013c130869cf4107d03150c8470a58
yaboi <tyler@tylergoelz.com>
2014-12-19 21:18:30 +0000

    Comment out html5Mode code to for temp fix of URL routing. Issue #1

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+/**
+ * @ngdoc overview
+ * @name passionDrivenPortfolio
+ * @description
+ * # passionDrivenPortfolio
+ *
+ * Main module of the application.
+ */
+angular.module('passionDrivenPortfolio', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    
+    'ui.bootstrap',
+    'ngFitText',
+
+    'app.controllers'
+
+]).config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainController'
+    }).when('/traveler', {
+        templateUrl: 'views/traveler.html',
+        controller: 'TravelerController'
+    }).when('/developer', {
+        templateUrl: 'views/developer.html',
+        controller: 'DeveloperController'
+    }).when('/404', {
+        templateUrl: 'views/404.html'
+    }).otherwise({
+        redirectTo: '/404'
+    });
+    
+//     $locationProvider.html5Mode(true);
+});
