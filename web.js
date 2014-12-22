44fc2bad0f382755246787f88321a3cb6c61a2c1
yaboi <tyler@tylergoelz.com>
2014-12-22 20:54:40 +0000

    Change <base> to point to /dist in attempts to fix URL Routing. Issue #1

--- web.js
+++ web.js
@@ -4,14 +4,23 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
-// app.use(function(req, res, next) {
-//     if (req.headers['x-forwarded-proto'] != 'https') {
-//         res.redirect('https://' + req.headers.host + req.path);
-//     }
-//     else {
-//         return next();
-//     }
+
+// app.use('/views', express.static(__dirname + '/views'));
+
+// app.get('/views', function (req, res) {
+//   var name = req.params.name;
+//   res.render('views/' + name);
 // });
+
+// app.all('/*', function(req, res, next) {
+//     res.sendfile('index.html', { root: __dirname });
+// });
+
+// app.all('*', function(req, res, next) {
+//     res.sendfile('index.html', { root: __dirname });
+// });
+
+
 app.use(morgan('app'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
