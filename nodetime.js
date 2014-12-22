62ca994cf0b5225dc5d4b2fbbc39367997aea089
yaboi <tyler@tylergoelz.com>
2014-12-22 20:58:17 +0000

    Update app name in bower.json and package.json

--- nodetime.js
+++ nodetime.js
@@ -1,6 +1,6 @@
 if(process.env.NODETIME_ACCOUNT_KEY) {
   require('nodetime').profile({
     accountKey: process.env.NODETIME_ACCOUNT_KEY,
-    appName: 'Tyler Goelz Staging'
+    appName: 'Passion Driven Portfolio'
   });
 }
