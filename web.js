c63df9afc99ca9cc7e34ea18a4ac775ad043a143
yaboi <tyler@tylergoelz.com>
2014-12-22 21:47:43 +0000

    Fix #1: Remove Html5Mode, rewrite routing, and fix direct link loading.

--- web.js
+++ web.js
@@ -5,22 +5,6 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
-// app.use('/views', express.static(__dirname + '/views'));
-
-// app.get('/views', function (req, res) {
-//   var name = req.params.name;
-//   res.render('views/' + name);
-// });
-
-// app.all('/*', function(req, res, next) {
-//     res.sendfile('index.html', { root: __dirname });
-// });
-
-// app.all('*', function(req, res, next) {
-//     res.sendfile('index.html', { root: __dirname });
-// });
-
-
 app.use(morgan('app'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
